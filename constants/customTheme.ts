1ee31ab4112708ce1ee9e8058dfe379b2d94a5c5
BasselMalek <bassel.walid04@gmail.com>
2025-10-14 17:15:45 +0300

    TODOS

--- constants/customTheme.ts
+++ constants/customTheme.ts
@@ -1,4 +1,8 @@
 const lightPalette = {
+    //TODO: 1. Fix the color of NFC save button.
+    //TODO: 2. Adjust the contrast in tags/buttons.
+    //TODO: 2. Integrate the tertiary color into the gradient more.
+
     colors: {
         primary: "rgb(0, 63, 119)",
         onPrimary: "rgb(255, 255, 255)",
