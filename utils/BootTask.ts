bf0449412a12c1d0c18549d316be9d89fb6cb5f4
BasselMalek <bassel.walid04@gmail.com>
2025-10-28 16:33:33 +0300

    chore: add debug message to HeadlessJS task.

--- utils/BootTask.ts
+++ utils/BootTask.ts
@@ -7,17 +7,17 @@ import { setLinkingScheme } from "@/modules/expo-alarm-manager";
 const BootTask = async () => {
     let db;
     try {
-        //TODO: should i move the linking scheme to some sort of storage?
         setLinkingScheme("nudge://alarms");
         db = await s.openDatabaseAsync("nudge_alarms.db");
         const rows = await db.getAllAsync<AlarmDto>(
             "SELECT * FROM alarms WHERE is_enabled = 1"
         );
         const result = await rescheduleAllForOnBoot(rows.map(parseAlarm));
+        console.log(result);
     } catch (e) {
         console.log(e);
     } finally {
-        db?.closeAsync();
+        await db?.closeAsync();
     }
     return;
 };
